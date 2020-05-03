3150ccd3332fac1f88eb037a756b5afff18c7437
BMatrix <ben_vinck@yahoo.com>
2020-05-03 22:20:56 +0200

    Added Team page content
    Added photos of the team members
    Added Member.vue
    Filled in Team page content (placeholder text)

---
+++ src/components/Member.vue
@@ -0,0 +1,76 @@
+<template>
+  <div :class="['member', reverse ? 'reverse' : '']">
+    <v-avatar size="200px" :class="['avatar']">
+      <img :src="image" />
+      <div :class="['grayscale', redscale ? 'redscale' : '']"></div>
+    </v-avatar>
+    <div id="text" :class="[reverse ? 'righttext' : 'lefttext']">
+      <h2>
+        <slot name="name"></slot>
+      </h2>
+      <h3>
+        <slot name="role"></slot>
+      </h3>
+      <p>
+        <slot name="description"></slot>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.member {
+  display: flex;
+  flex-direction: row;
+}
+.reverse {
+  flex-direction: row-reverse;
+}
+
+.grayscale {
+  position: absolute;
+  width: inherit;
+  height: inherit;
+  transition: all 0.3s ease-in-out;
+  background-color: #777777a2;
+}
+.grayscale:hover {
+  transition: all 0.3s ease-in-out;
+  background-color: #00000000;
+}
+.redscale {
+    background-color: #f73b3ba2;
+}
+
+#text {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 50%;
+}
+.lefttext {
+  text-align: left;
+  margin-left: 5%;
+}
+.righttext {
+  text-align: right;
+  margin-right: 5%;
+}
+</style>
+
+<script>
+export default {
+  name: "Member",
+  props: {
+    image: String,
+    reverse: {
+      type: Boolean,
+      default: false
+    },
+    redscale: {
+        type: Boolean,
+        default: false
+    }
+  }
+};
+</script>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="team">
-    <h1>Team Page</h1>
+    <div class="center">
+      <h1>The Team</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur laborum adipisci, voluptatum laboriosam nesciunt ex commodi voluptatem similique accusamus totam cupiditate sint, voluptas harum, iusto tempora libero reiciendis distinctio alias!</p>
+      <div id="members">
+        <member class="member"                   :redscale="true"  :image="'team/AleksandraLobka.jpg'">    <template v-slot:name>Aleksandra Lobka</template>     <template v-slot:role>Architecture Engineering</template>   <template v-slot:description>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, eos voluptates explicabo dolor delectus enim debitis similique maxime assumenda pariatur ad vero optio earum, tempore numquam placeat atque distinctio accusamus.</template></member>
+        <member class="member"  :reverse="true"                    :image="'team/BenVinck.jpg'">           <template v-slot:name>Ben Vinck</template>            <template v-slot:role>Electronics and ICT</template>        <template v-slot:description>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, eos voluptates explicabo dolor delectus enim debitis similique maxime assumenda pariatur ad vero optio earum, tempore numquam placeat atque distinctio accusamus.</template></member>
+        <member class="member"                                     :image="'team/DarioVanMechelen.png'">   <template v-slot:name>Dario Van Mechelen</template>   <template v-slot:role>Graphic and Digital Media</template>  <template v-slot:description>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, eos voluptates explicabo dolor delectus enim debitis similique maxime assumenda pariatur ad vero optio earum, tempore numquam placeat atque distinctio accusamus.</template></member>
+        <member class="member"  :reverse="true"  :redscale="true"  :image="'team/EricGonzales.jpg'">       <template v-slot:name>Eric Gonzales</template>        <template v-slot:role>Industrial Design</template>          <template v-slot:description>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, eos voluptates explicabo dolor delectus enim debitis similique maxime assumenda pariatur ad vero optio earum, tempore numquam placeat atque distinctio accusamus.</template></member>
+        <member class="member"                   :redscale="true"  :image="'team/JoyAgwunedu.jpg'">        <template v-slot:name>Joy Agwunedu</template>         <template v-slot:role>Multimedia Production</template>      <template v-slot:description>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, eos voluptates explicabo dolor delectus enim debitis similique maxime assumenda pariatur ad vero optio earum, tempore numquam placeat atque distinctio accusamus.</template></member>
+        <member class="member"  :reverse="true"                    :image="'team/MadelinaGavril.jpg'">     <template v-slot:name>Madelina Gavril</template>      <template v-slot:role>Advertising</template>                <template v-slot:description>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, eos voluptates explicabo dolor delectus enim debitis similique maxime assumenda pariatur ad vero optio earum, tempore numquam placeat atque distinctio accusamus.</template></member>
+        <member class="member"                   :redscale="true"  :image="'team/MarioGomezCarnero.jpg'">  <template v-slot:name>Mario Gomez Carnero</template>  <template v-slot:role>Industrial Design</template>          <template v-slot:description>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, eos voluptates explicabo dolor delectus enim debitis similique maxime assumenda pariatur ad vero optio earum, tempore numquam placeat atque distinctio accusamus.</template></member>
+        <member class="member"  :reverse="true"                    :image="'team/Sara-GülKangöz.jpg'">     <template v-slot:name>Sara-Gül Kangöz</template>      <template v-slot:role>Media and Communications</template>   <template v-slot:description>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, eos voluptates explicabo dolor delectus enim debitis similique maxime assumenda pariatur ad vero optio earum, tempore numquam placeat atque distinctio accusamus.</template></member>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.center {
+  margin-top: 1em;
+  margin-left: auto;
+  margin-right: auto;
+  width: 70%;
+  text-align: center;
+}
+#members{
+  margin-top: 3em;
+}
+</style>
+
+<script>
+import Member from "../components/Member.vue";
+export default {
+  components: {
+    Member
+  }
+};
+</script>
